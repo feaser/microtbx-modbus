26149037a2ca7c41be5392d0f0f5f3e06affe510
Frank Voorburg <voorburg@feaser.com>
2023-02-11 19:21:35 +0100

    Minor refactoring.

--- source/tbxmodbusuart.c
+++ source/tbxmodbusuart.c
@@ -34,9 +34,9 @@
 
 /************************************************************************************//**
 ** \brief     Initializes the UART channel.
-** \param     serial_port The serial port to use in the range 1 .. max supported serial
-**            ports. The actual meaning of the serial port is hardware dependent. It
-**            typically maps to the UART peripheral number. E.g. 1 = USART1 on an STM32.
+** \param     serial_port The serial port to use in the range 1..8 ports. The actual
+**            meaning of the serial port is hardware dependent. It typically maps to the
+**            UART peripheral number. E.g. 1 = USART1 on an STM32.
 ** \param     baudrate The desired communication speed.
 ** \param     databits Number of databits for a character.
 ** \param     stopbits Number of stop bits at the end of a character.
@@ -55,10 +55,10 @@ void TbxModbusUartInit(uint8_t serial_port,
   TBX_UNUSED_ARG(parity);
 
   /* Verify parameters. */
-  TBX_ASSERT(serial_port > 0U);
+  TBX_ASSERT((serial_port >= 1U) && (serial_port <= 8U));
 
   /* Only continue with valid parameters. */
-  if (serial_port > 0U)
+  if ((serial_port >= 1U) && (serial_port <= 8U))
   {
     /* TODO Implement TbxModbusUartInit(). */
   }
