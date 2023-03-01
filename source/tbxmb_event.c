fcda4fbae85f892f7dd6641deeb2aae7f3533548
Frank Voorburg <voorburg@feaser.com>
2023-03-01 20:14:44 +0100

    Implemented event poller list.

--- source/tbxmb_event.c
+++ source/tbxmb_event.c
@@ -74,45 +74,93 @@ typedef struct
 ****************************************************************************************/
 void TbxMbEventTask(void)
 {
-  static const uint16_t defaultWaitTimeoutMs = 5000U;
-  static uint16_t       waitTimeout = defaultWaitTimeoutMs;
-  tTbxMbEvent           newEvent = { 0 };
+  static tTbxList       * pollerList = NULL;
+  static uint8_t          pollerListInitialized = TBX_FALSE;
+  static const uint16_t   defaultWaitTimeoutMs = 5000U;
+  static uint16_t         waitTimeoutMS = defaultWaitTimeoutMs;
+  tTbxMbEvent             newEvent = { 0 };
 
-  /* Wait for a new event to be posted to the event queue. */
-  if (TbxMbOsalWaitEvent(&newEvent, waitTimeout) == TBX_TRUE)
+  /* Only initialize the event poller once, */
+  if (pollerListInitialized == TBX_FALSE)
   {
-    /* Filter on the event identifier. */
-    switch (newEvent.id)
-    {
-    case TBX_MB_EVENT_ID_START_POLLING:
-      /* TODO Add context to the EventPoller linked list. */
-      break;
-    
-    case TBX_MB_EVENT_ID_STOP_POLLING:
-      /* TODO Remove context from the EventPoller linked list. */
-      break;
+    pollerListInitialized = TBX_TRUE;
+    /* Ceate the queue for storing context of which the pollFcn should be called. */
+    pollerList = TbxListCreate();
+    /* Verify that the queue creation succeeded. If this assertion fails, increase the
+     * heap size using configuration macro TBX_CONF_HEAP_SIZE.
+     */
+    TBX_ASSERT(pollerList != NULL);
+  }
 
-    default:
-      /* Check the opaque context pointer. */
-      TBX_ASSERT(newEvent.context != NULL);
-      /* Only continue with a valid opaque context pointer. */
-      if (newEvent.context != NULL)
+  /* Wait for a new event to be posted to the event queue. Note that that wait time only
+   * applies in case an RTOS is configured for the OSAL. Otherwise (TBX_MB_OPT_OSAL_NONE)
+   * this function returns immediately.
+   */
+  if (TbxMbOsalWaitEvent(&newEvent, waitTimeoutMS) == TBX_TRUE)
+  {
+    /* Check the opaque context pointer. */
+    TBX_ASSERT(newEvent.context != NULL);
+    /* Only continue with a valid opaque context pointer. */
+    if (newEvent.context != NULL)
+    {
+      /* Filter on the event identifier. */
+      switch (newEvent.id)
       {
-        /* Convert the opaque pointer to the event context structure. */
-        tTbxMbEventCtx * event_ctx = (tTbxMbEventCtx *)newEvent.context;
-        /* Pass the event on to the context's event processor. */
-        if (event_ctx->processFcn != NULL)
+        case TBX_MB_EVENT_ID_START_POLLING:
         {
-          event_ctx->processFcn(&newEvent);
+          /* Add the context at the end of the event poller list. */
+          uint8_t insertResult = TbxListInsertItemBack(pollerList, newEvent.context);
+          /* Check that the item could be added to the queue. If not, then the heaps size
+           * is configured too small. In this case increase the heap size using
+           * configuration macro TBX_CONF_HEAP_SIZE. 
+           */
+          TBX_ASSERT(insertResult == TBX_OK);
         }
+        break;
+      
+        case TBX_MB_EVENT_ID_STOP_POLLING:
+        {
+          /* Remove the context from the event poller list. */
+          TbxListRemoveItem(pollerList, newEvent.context);
+        }
+        break;
+
+        default:
+        {
+          /* Convert the opaque pointer to the event context structure. */
+          tTbxMbEventCtx * eventCtx = (tTbxMbEventCtx *)newEvent.context;
+          /* Pass the event on to the context's event processor. */
+          if (eventCtx->processFcn != NULL)
+          {
+            eventCtx->processFcn(&newEvent);
+          }
+        }
+        break;
       }
-      break;
     }
   }
 
-  /* TODO Iterate over EventPoller linked list and call their pollFcn if not NULL. If
-   *      the linked list is not empty, make sure to set waitTimeout to 1 via a const.
+  /* Iterate over the event poller list. */
+  void * listItem = TbxListGetFirstItem(pollerList);
+  while (listItem != NULL)
+  {
+    /* Convert the opaque pointer to the event context structure. */
+    tTbxMbEventCtx * eventPollCtx = (tTbxMbEventCtx *)listItem;
+    /* Call its poll function if configured. */
+    if (eventPollCtx->pollFcn != NULL)
+    {
+      eventPollCtx->pollFcn(listItem);
+      /* Move on to the next item in the list. */
+      listItem = TbxListGetNextItem(pollerList, listItem);
+    }
+  }
+
+  /* Set the event wait timeout for the next call to this task function. If the event
+   * poller list is not empty, keep the wait time short to make sure the poll functions
+   * get continuously called. Otherwise go back to the default wait time to not hog up
+   * CPU time unnecessarily.
    */
+  waitTimeoutMS = (TbxListGetSize(pollerList) > 0U) ? 1U : defaultWaitTimeoutMs;
 } /*** end of TbxMbEventTask ***/
 
 
